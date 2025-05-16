08672638a1d995dabe4aff04484cfb0f3d113237
GaiLuyang <823695228@qq.com>
2025-05-16 17:06:33 +0800

    Add files via upload

--- experiment/FL.py
+++ experiment/FL.py
@@ -44,23 +44,14 @@ def server_update_fedavg(clients_list = None,
             bit += clients_list[i].model.param_num * task.b + 32
             clients_list[i].Ran = Q_Range(clients_list[i], clients_list[i].model.model_body, server.model.model_k, clients_list[i].model.param_num, task)
             energy += (task.tao ** 2) * (clients_list[i].loc[0]**2) * task.N0 * task.Bn * (2 ** ((task.b * clients_list[i].model.param_num + 32) / task.tao / task.Bn) -1)
-        elif task.opt == 'FedRQ' or task.opt == 'FedSQ' or task.opt == 'FedPAQ':
-            bit += clients_list[i].model.param_num * task.b
-            clients_list[i].Ran = Q_Range(clients_list[i], clients_list[i].model.model_body, server.model.model_k, clients_list[i].model.param_num, task)
-            energy += (task.tao ** 2) * (clients_list[i].loc[0]**2) * task.N0 * task.Bn * (2 ** ((task.b * clients_list[i].model.param_num + 32) / task.tao / task.Bn) -1)
-        elif task.opt == 'Fedavg':
-            bit += clients_list[i].model.param_num * task.b
-            energy += (task.tao ** 2) * (clients_list[i].loc[0]**2) * task.N0 * task.Bn *  (2 ** ((task.b * clients_list[i].model.param_num ) / task.tao / task.Bn) -1)
         
         for param_server, param_server_model_k, param_client in zip(server.model.model_body.parameters(), 
                                                                     server.model.model_k.parameters(),
                                                                    clients_list[i].model.model_body.parameters()):
             cal3 = time.time()
-            if task.opt == 'FedRQ' or task.opt == 'FedSQ' or task.opt == 'FedPAQ' or task.opt == 'FedBIQ' or task.opt == 'FedWBIQ':
+            if  task.opt == 'FedBIQ' or task.opt == 'FedWBIQ':
                 param = param_client.data - param_server_model_k.data
                 param = QDP.Quantization(param, param_server_model_k.data * 0, clients_list[i].Ran, task)
-            elif task.opt == 'Fedavg':
-                param = param_client.data - param_server_model_k.data
             cal4 = time.time()
             cal += cal4 - cal3 
             param_server.data += 1 / len(train_clients_list) * param  
@@ -146,15 +137,7 @@ def norm_l2(a_model_body, b_model_body = None, m = None):
     return torch.sqrt(l2)
 
 def Q_Range(client, client_model_body, server_model_k, param_num, task): 
-    if task.opt == 'FedBIQ' or task.opt == 'FedWBIQ':
-        Ran =  task.ex * min(math.sqrt(12*2^(2*task.b))*norm_l2(client_model_body, server_model_k).item()/param_num, client.Ran)
-    elif task.opt == 'FedRQ':
-        Ran = task.ex * task.RQ_Ran
-    elif task.opt == 'FedSQ':
-        Ran = task.ex * task.SQ_Ran
-    elif task.opt == 'FedPAQ':
-        task.PAQ_norm = task.PAQ_Ran 
-        Ran = task.ex * task.PAQ_Ran
+    Ran =  task.ex * min(math.sqrt(12*2^(2*task.b))*norm_l2(client_model_body, server_model_k).item()/param_num, client.Ran)
     return Ran
 
 def Fed_fl(clients_list, server, task):
